d9e840d41b373aa7d6b803352cdc4da4e6b77351
Ivan Lifanov <aryre4er@yandex.ru>
2025-01-24 08:57:10 +0300

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default config = {
+/** @type {import('eslint').Linter.Config} */
+const config = {
   languageOptions: { globals: globals.node },
   ...pluginJs.configs.recommended,
 };
+
+export default config;
